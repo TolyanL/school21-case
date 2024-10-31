1d465c5d0b0265ec0e58ecc8a54758e79465ef77
kekcek1 <utubkekcek@gmail.com>
2024-10-31 16:23:19 +0300

    make create my group page

--- school21_case/groups/urls.py
+++ school21_case/groups/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import my_groups
+from .views import my_groups, create_group
+
 
 urlpatterns = [
     path("my-groups", my_groups, name="my_groups"),
+    path("create-group", create_group, name="create_group"),
 
 ]
--- school21_case/groups/views.py
+++ school21_case/groups/views.py
@@ -4,3 +4,8 @@ from django.http import HttpRequest
 
 def my_groups(request: HttpRequest):
     return render(request, "groups/my_groups.html")
+
+
+def create_group(request: HttpRequest):
+    return render(request, "groups/create_group.html")
+    
