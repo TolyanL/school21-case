9c42b84e368d58d6d3ae80412588f21e46e8793b
Dino <daminalutov9@gmail.com>
2024-10-31 21:25:21 +0300

    create find_group page

--- school21_case/groups/urls.py
+++ school21_case/groups/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import my_groups
+from .views import my_groups, find_groups
 
 urlpatterns = [
     path("my-groups", my_groups, name="my_groups"),
-
+    path("find-groups", find_groups, name="find_groups"),
 ]
--- school21_case/groups/views.py
+++ school21_case/groups/views.py
@@ -1,6 +1,10 @@
+from django.contrib.admin.templatetags.admin_list import search_form
 from django.shortcuts import render
 from django.http import HttpRequest
-
+from users.forms import ProfileSearchForm
 
 def my_groups(request: HttpRequest):
-    return render(request, "groups/my_groups.html")
+    return render(request, "groups/my_groups.html", {"search_form": ProfileSearchForm()})
+
+def find_groups(request: HttpRequest):
+    return render(request, "groups/find_groups.html", {"search_form": ProfileSearchForm()})
