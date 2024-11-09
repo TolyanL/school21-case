b09232990b5f8a8098ca68f443d9c032957396fe
kekcek1 <utubkekcek@gmail.com>
2024-11-09 12:08:52 +0300

    create about and help urls

--- school21_case/home/urls.py
+++ school21_case/home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, about, help_
 
 urlpatterns = [
     path("", home, name="home"),
+    path("about", about, name="about"),
+    path("help", help_, name="help"),
 ]
